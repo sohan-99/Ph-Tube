702453ccc442266f4e8972c3673d0c863169e69b
sohan-99 <sohan75632@gmail.com>
2023-09-02 00:21:15 +0600

    dynamic button done

--- script/tube.js
+++ script/tube.js
@@ -1,52 +1,65 @@
 const displayvideos = videos => {
     // step --- 1
-
     const videoContainer = document.getElementById('videos-container');
-
+    videoContainer.innerHTML='';
 
     videos.forEach(video => {
         console.log(video);
         // step-2 create a div 
         const videoCard = document.createElement('div');
-        videoCard.classList = `card w-96 bg-black-400 shadow-xl`;
+        videoCard.classList = `card w-83 bg-black-400 shadow-xl`;
         // step-3 inner html 
         videoCard.innerHTML = `
-    
-        <figure><img class="" src="${video.thumbnail}" alt="Shoes" /></figure>
+        <figure><img class="h-[250px]" src="${video.thumbnail}" alt="Shoes" /></figure>
         <div class="card-body">
         <div class="grid grid-cols-2">
-        <img src="${video.authors.profile_picture}" alt="Shoes" />
+        <img class="h-11 w-11 rounded-full " src="${video.authors[0].profile_picture}" alt="Shoes" />
             <h2 class="card-title">${video.title}</h2>
-          
         </div>
-        <h2 class="card-title2">${video.authors.profile_name}</h2>
-        
+        <h2 class="card-title2">${video.others.views}</h2>
+        <img src="" alt="" />
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
-            </div>
-        </div>
-        
-        `;
-        // step-4 appendchile 
-
+            
+        </div>`;
         videoContainer.appendChild(videoCard);
-
     });
 };
 
-const handleChatagory = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/videos/category/1000");
+const handleChatagory  = async (id) => {
+    console.log(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
     const videos = data.data;
-
+    
     displayvideos(videos);
 };
 
-handleChatagory();
+handleChatagory('1000');
 
+const displaybutton = button => {
+    // step --- 1
+    const menucontainer = document.getElementById('menu-container');
+    
+    button.forEach(menu => {
+        let menuBar = document.createElement('div');
+        menuBar.classList = `text-center  flex `;
+        menuBar.innerHTML = `
+        <div onclick="handleChatagory(${menu.category_id})" class=" block  h-[35px] rounded w-[75px] text-lg font-semibold text-[#FFF]  bg-[#FF1F3D]">${menu.category}</div>
+        
+        `;
 
+        menucontainer.appendChild(menuBar);
+    });
 
+};
+const handlemenu = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await res.json();
+    const button = data.data
+    displaybutton(button);
+
+};
+handlemenu()
 
 
 
